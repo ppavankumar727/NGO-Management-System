b499e5130521ced8c9a7b0b702ff45bd021abf98
PULLUR PAVAN KUMAR <ppavankumar727@gmail.com>
2020-12-06 16:30:11 +0530

    login completed

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 session_start();
 
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    if(isset($_SESSION['role'])){
+        echo "<a href='logout.php'>Logout</a>";
+}
+ ?>
+</body>
+</html>
