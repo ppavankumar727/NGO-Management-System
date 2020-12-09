85b7cfb5269e49fb4088733c4298adf09c263cfe
PULLUR PAVAN KUMAR <ppavankumar727@gmail.com>
2020-12-09 16:36:11 +0530

    completed basic functioning styling yet to be done

--- admin/delete.php
+++ admin/delete.php
@@ -1,15 +1,23 @@
 
 <?php 
-
-if(!isset($_SESSION['donor_id'])){
+session_start();
+require_once "../pdo.php";
+if(!isset($_SESSION['admin_id'])){
     die("Login first");
+}
+if(!isset($_GET['donor_id'])){
+    die("No param");
 }
 if(isset($_POST['cancel'])){
     header('Location: ../index.php');
-    return;
+    return;   
 }
-
-
+else if(isset($_POST['submit'])){
+$stmt3 = $pdo->query( "DELETE FROM `ngo_account` WHERE `ngo_account`.`donor_id` =".$_GET['donor_id']);
+$rows2 = $stmt3->fetchAll(PDO::FETCH_ASSOC);
+header('Location: ../index.php');
+return;  
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +28,13 @@ if(isset($_POST['cancel'])){
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <form action="" method="post">
+        <p>Are You You Want To DELETE
+        <br>
+        <input type="submit" name ="submit"value="Submit">
+            <input type="submit" name="cancel" value="Cancel"></p>
+        </form>
+    </div>
 </body>
 </html>
--- adminIndex.php
+++ adminIndex.php
@@ -1,5 +1,5 @@
 <?php 
-$stmt3 = $pdo->query("SELECT donor.name , ngo_account.donationS FROM donor JOIN ngo_account WHERE donor.donor_id = ngo_account.donor_id");
+$stmt3 = $pdo->query("SELECT ngo_account.donor_id , donor.name , ngo_account.donationS FROM donor JOIN ngo_account WHERE donor.donor_id = ngo_account.donor_id");
 $rows = $stmt3->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <div>
@@ -10,6 +10,8 @@ foreach($rows as $row){
     echo "<li>";
     echo htmlentities($row['name']);
     echo " ".htmlentities($row['donationS']);
+    echo(" <a href='admin/delete.php?donor_id=".$row['donor_id']."'>Delete</a>");
+
     echo "</li>";
 }
 ?>
