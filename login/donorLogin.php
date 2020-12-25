24b9b423dde479c2eca6019832e2d3ebc65427e5
PULLUR PAVAN KUMAR <ppavankumar727@gmail.com>
2020-12-25 14:48:59 +0530

    updated flash messages of login pages

--- login/donorLogin.php
+++ login/donorLogin.php
@@ -34,13 +34,17 @@ if ( isset($_POST['username'])) {
 
 <?php
     if(isset($_SESSION['success'])){
+        echo '<div class="alert alert-success" role="alert">';
         echo $_SESSION['success'];
         unset ($_SESSION['success']);
+        echo '</div>' ;
     }
 
     if(isset($_SESSION['error'])){
+        echo '<div class="alert alert-danger" role="alert">';
         echo $_SESSION['error'];
         unset($_SESSION['error']);
+        echo '</div>';
     }
 ?>
 
